019e8b17a1cf3117716cd3794925e6e5ae291491
Sakura Mo <moyinzi@outlook.com>
2019-03-29 22:13:38 +0800

    LDT

--- os/src/os002/boot/setup.asm
+++ os/src/os002/boot/setup.asm
@@ -12,7 +12,7 @@ mov		sp, offset
 ; 设置回跳段地址
 mov		[LABEL_GO_BACK_TO_REAL+3], ax
 
-;jmp		go_to_protect
+jmp		go_to_protect
 
 ;mov     al, 0x13           ; 显示器模式
 ;mov     ah, 0x00           ; BIOS中断-设置显示器模式
@@ -47,6 +47,11 @@ InitGDT LABEL_DATA, LABEL_DESC_DATA
 ; 初始化堆栈段描述符
 InitGDT LABEL_STACK, LABEL_DESC_STACK
 
+; 初始化 LDT 在 GDT 中的描述符
+InitGDT LABEL_LDT, LABEL_DESC_LDT
+; 初始化 LDT 中的描述符
+InitGDT LABEL_CODE_A, LABEL_LDT_DESC_CODEA
+
 ; 为加载 GDTR 作准备 & 加载 GDTR & 关中断 & 打开 A20
 LoadGDT LABEL_GDT, GdtPtr
 EnableA20
@@ -68,15 +73,14 @@ mov		es, ax
 mov		ss, ax
 mov		sp, [SPValueInRealMode]
 DisableA20
-finx:
-LoopHLT finx
+LoopHLT
 
 
 
 ; 32 位代码段. 由实模式跳入.
 [BITS	32]
 LABEL_SEG_CODE32:
-	jmp test_videoX
+	;jmp test_videoX
 
 	mov	ax, SelectorData
 	mov	ds, ax			; 数据段选择子
@@ -94,20 +98,28 @@ LABEL_SEG_CODE32:
 	mov	esi, OffsetPMMessage	; 源数据偏移
 	mov	edi, (80 * 10 + 0) * 2	; 目的数据偏移。屏幕第 10 行, 第 0 列。
 	cld
-;;.1:
-;;	lodsb
-;;	test	al, al
-;;	jz	.2
-;;	mov	[gs:edi], ax
-;;	add	edi, 2
-;;	jmp	.1
-;;.2:	; 显示完毕
+.1:
+	lodsb
+	test	al, al
+	jz	.2
+	mov	[gs:edi], ax
+	add	edi, 2
+	jmp	.1
+.2:	; 显示完毕
 	call	DispReturn
+
 	call	TestRead
 	call	TestWrite
 	call	TestRead
+
+	; Load LDT
+	mov	ax, SelectorLDT
+	lldt ax
+	jmp	SelectorLDTCodeA:0		; 跳入局部任务
+
 	; 到此停止
-	jmp	SelectorCode16:0
+	; jmp	SelectorCode16:0
+
 TestRead:
 	xor	esi, esi
 	mov	ecx, 8
@@ -118,6 +130,7 @@ TestRead:
 	loop	.loop
 	call	DispReturn
 	ret
+
 TestWrite:
 	push	esi
 	push	edi
@@ -136,6 +149,7 @@ TestWrite:
 	pop	edi
 	pop	esi
 	ret
+
 DispAL:
 	push	ecx
 	push	edx
@@ -161,6 +175,7 @@ DispAL:
 	pop	edx
 	pop	ecx
 	ret
+
 DispReturn:
 	push	eax
 	push	ebx
@@ -217,3 +232,36 @@ Code16Len	equ	$-LABEL_SEG_CODE16
 
 
 %include	"gdt.inc"
+
+
+
+; LDT
+ALIGN	32
+[BITS	16]
+;                            段基址       段界限      属性
+LABEL_LDT:
+; Code, 32 位
+LABEL_LDT_DESC_CODEA: Descriptor 0, CodeALen - 1, DA_C + DA_32
+
+LDTLen		equ	$-LABEL_LDT
+
+; LDT 选择子
+SelectorLDTCodeA	equ	LABEL_LDT_DESC_CODEA	- LABEL_LDT + SA_TIL
+
+
+
+; CodeA (LDT, 32 位代码段)
+ALIGN	32
+[BITS	32]
+LABEL_CODE_A:
+	mov	ax, SelectorVideo
+	mov	gs, ax			; 视频段选择子(目的)
+
+	mov	edi, (80 * 13 + 0) * 2	; 屏幕第 11 行, 第 0 列。
+	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
+	mov	al, 'L'
+	mov	[gs:edi], ax
+
+	; 准备经由16位代码段跳回实模式
+	jmp	SelectorCode16:0
+CodeALen	equ	$-LABEL_CODE_A
