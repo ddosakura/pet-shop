ae102e41731433dc5d75d5bde1e7c959617a71e1
Sakura Mo <moyinzi@outlook.com>
2019-04-01 23:03:01 +0800

    memory size

--- os/src/os003/boot/setup.asm
+++ os/src/os003/boot/setup.asm
@@ -23,6 +23,24 @@ mov		[LABEL_GO_BACK_TO_REAL+3], ax
 
 mov		[SPValueInRealMode], sp
 
+; 得到内存数
+mov	ebx, 0
+mov	di, _MemChkBuf
+.loop:
+	mov	eax, 0E820h
+	mov	ecx, 20
+	mov	edx, 0534D4150h
+	int	15h
+	jc	LABEL_MEM_CHK_FAIL
+	add	di, 20
+	inc	dword [_dwMCRNumber]
+	cmp	ebx, 0
+	jne	.loop
+	jmp	LABEL_MEM_CHK_OK
+LABEL_MEM_CHK_FAIL:
+	mov	dword [_dwMCRNumber], 0
+LABEL_MEM_CHK_OK:
+
 ; 初始化 16 位代码段描述符
 InitGDT LABEL_SEG_CODE16, LABEL_DESC_CODE16
 ; 初始化 32 位代码段描述符
@@ -61,10 +79,10 @@ LoopHLT
 ; 32 位代码段. 由实模式跳入.
 [BITS	32]
 LABEL_SEG_CODE32:
-	call SetupPaging
-
 	mov	ax, SelectorData
 	mov	ds, ax			; 数据段选择子
+	mov	ax, SelectorData
+	mov	es, ax
 	mov	ax, SelectorVideo
 	mov	gs, ax			; 视频段选择子
 	mov	ax, SelectorStack
@@ -72,31 +90,38 @@ LABEL_SEG_CODE32:
 	mov	esp, TopOfStack
 
 	; 下面显示一个字符串
-	mov	ah, 0Ch			; 0000: 黑底    1100: 红字
-	xor	esi, esi
-	xor	edi, edi
-	mov	esi, OffsetPMMessage	; 源数据偏移
-	mov	edi, (80 * 10 + 0) * 2	; 目的数据偏移。屏幕第 10 行, 第 0 列。
-	cld
-.1:
-	lodsb
-	test	al, al
-	jz	.2
-	mov	[gs:edi], ax
-	add	edi, 2
-	jmp	.1
-.2:	; 显示完毕
+	push	szPMMessage
+	call	DispStr
+	add	esp, 4
+
+	push	szMemChkTitle
+	call	DispStr
+	add	esp, 4
+
+	call	DispMemSize		; 显示内存信息
+	call	SetupPaging		; 启动分页机制
 
 	; 到此停止
 	jmp	SelectorCode16:0
 
 SetupPaging:
-	; 为简化处理, 所有线性地址对应相等的物理地址.
+	; 根据内存大小计算应初始化多少PDE以及多少页表
+	xor	edx, edx
+	mov	eax, [dwMemSize]
+	mov	ebx, 400000h	; 400000h = 4M = 4096 * 1024, 一个页表对应的内存大小
+	div	ebx
+	mov	ecx, eax	; 此时 ecx 为页表的个数，也即 PDE 应该的个数
+	test	edx, edx
+	jz	.no_remainder
+	inc	ecx		; 如果余数不为 0 就需增加一个页表
+.no_remainder:
+	push	ecx		; 暂存页表个数
+
+	; 为简化处理, 所有线性地址对应相等的物理地址. 并且不考虑内存空洞.
 
 	; 首先初始化页目录
 	mov	ax, SelectorPageDir	; 此段首地址为 PageDirBase
 	mov	es, ax
-	mov	ecx, 1024		; 共 1K 个表项
 	xor	edi, edi
 	xor	eax, eax
 	mov	eax, PageTblBase | PG_P  | PG_USU | PG_RWW
@@ -105,10 +130,13 @@ SetupPaging:
 	add	eax, 4096		; 为了简化, 所有页表在内存中是连续的.
 	loop	.1
 
-	; 再初始化所有页表 (1K 个, 4M 内存空间)
+	; 再初始化所有页表
 	mov	ax, SelectorPageTbl	; 此段首地址为 PageTblBase
 	mov	es, ax
-	mov	ecx, 1024 * 1024	; 共 1M 个页表项, 也即有 1M 个页
+	pop	eax			; 页表个数
+	mov	ebx, 1024		; 每个页表 1024 个 PTE
+	mul	ebx
+	mov	ecx, eax		; PTE个数 = 页表个数 * 1024
 	xor	edi, edi
 	xor	eax, eax
 	mov	eax, PG_P  | PG_USU | PG_RWW
@@ -127,6 +155,53 @@ SetupPaging:
 	nop
 
 	ret
+
+DispMemSize:
+	push	esi
+	push	edi
+	push	ecx
+
+	mov	esi, MemChkBuf
+	mov	ecx, [dwMCRNumber];for(int i=0;i<[MCRNumber];i++)//每次得到一个ARDS
+.loop:				  ;{
+	mov	edx, 5		  ;  for(int j=0;j<5;j++) //每次得到一个ARDS中的成员
+	mov	edi, ARDStruct	  ;  {//依次显示BaseAddrLow,BaseAddrHigh,LengthLow,
+.1:				  ;             LengthHigh,Type
+	push	dword [esi]	  ;
+	call	DispInt		  ;    DispInt(MemChkBuf[j*4]); //显示一个成员
+	pop	eax		  ;
+	stosd			  ;    ARDStruct[j*4] = MemChkBuf[j*4];
+	add	esi, 4		  ;
+	dec	edx		  ;
+	cmp	edx, 0		  ;
+	jnz	.1		  ;  }
+	call	DispReturn	  ;  printf("\n");
+	cmp	dword [dwType], 1 ;  if(Type == AddressRangeMemory)
+	jne	.2		  ;  {
+	mov	eax, [dwBaseAddrLow];
+	add	eax, [dwLengthLow];
+	cmp	eax, [dwMemSize]  ;    if(BaseAddrLow + LengthLow > MemSize)
+	jb	.2		  ;
+	mov	[dwMemSize], eax  ;    MemSize = BaseAddrLow + LengthLow;
+.2:				  ;  }
+	loop	.loop		  ;}
+				  ;
+	call	DispReturn	  ;printf("\n");
+	push	szRAMSize	  ;
+	call	DispStr		  ;printf("RAM size:");
+	add	esp, 4		  ;
+				  ;
+	push	dword [dwMemSize] ;
+	call	DispInt		  ;DispInt(MemSize);
+	add	esp, 4		  ;
+
+	pop	ecx
+	pop	edi
+	pop	esi
+	ret
+
+; 库函数
+%include	"lib.inc"
 
 SegCode32Len	equ	$ - LABEL_SEG_CODE32
 
